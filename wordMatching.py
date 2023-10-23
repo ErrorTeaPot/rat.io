49d5e96d9a9eb1070d63bf7a2f2cfd9053f18553
glesaout <gabin.lesaout@etudiant.univ-rennes1.fr>
2023-10-24 00:45:25 +0200

    Reorganize folders

--- wordMatching.py
+++ wordMatching.py
@@ -1,14 +1,14 @@
 # Open the suffixes file and read the suffixes into a list
-with open('suffixes.txt', 'r') as f:
+with open('suffix/domainNames.txt', 'r') as f:
     suffixes = [line.strip() for line in f]
 
 # Open the gutenberg file and read the words into a list
-with open('dicFR.txt', 'r') as f:
+with open('dictionaries/dicFR.txt', 'r') as f:
     words = [line.strip() for line in f]
 
 # Find the words that end with any of the suffixes and add a space between the word and the suffix
 matched_words = [word + ' ' + suffix for word in words for suffix in suffixes if word.endswith(suffix)]
 
 # Write the matched words to a new file
-with open('matched_words.txt', 'w') as f:
+with open('output/matched_words.txt', 'w') as f:
     f.write('\n'.join(matched_words))
